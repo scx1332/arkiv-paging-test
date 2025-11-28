bd25f295a108d1627b0dc2afc4d57095adafda45
scx1332 <sieciech@gmail.com>
2025-11-28 17:34:27 +0100

    Added eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
@@ -12,6 +13,10 @@ export default [
         ecmaVersion: "latest",
         sourceType: "module",
         project: "./tsconfig.json",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
     },
     plugins: {
@@ -29,12 +34,12 @@ export default [
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-non-null-assertion": "off",
 
       // General JavaScript/TypeScript rules
-      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-console": "off",
       "no-debugger": "error",
-      "no-alert": "warn",
+      "no-alert": "off",
       "prefer-const": "error",
       "no-var": "error",
       "object-shorthand": "warn",
